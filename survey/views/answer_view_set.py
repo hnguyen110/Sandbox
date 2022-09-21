32da714a81649ecc2a9b0b0336f54bed5652c062
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-09-21 15:28:13 -0400

    Complete the functionality to let the staff to manage survey, question and add the answer to the question, need to change the permission to allow user that is in the assigned group to access to the survey

--- survey/views/answer_view_set.py
+++ survey/views/answer_view_set.py
@@ -3,12 +3,23 @@ from rest_framework.viewsets import ModelViewSet
 
 from survey.models.answer import Answer
 from survey.serializers.answer.base_answer_serializer import BaseAnswerSerializer
+from survey.serializers.answer.modify_answer_serializer import ModifyAnswerSerializer
 
 
 class AnswerViewSet(ModelViewSet):
-    queryset = Answer.objects.all()
     permission_classes = [IsAuthenticated]
-    serializer_class = BaseAnswerSerializer
+
+    def get_serializer_class(self):
+        if self.request.method in ['POST', 'PUT']:
+            return ModifyAnswerSerializer
+        return BaseAnswerSerializer
+
+    def get_queryset(self):
+        return Answer \
+            .objects \
+            .prefetch_related('solution') \
+            .prefetch_related('question') \
+            .filter(participant_id=self.kwargs['participant_pk'])
 
     def get_serializer_context(self):
         return {
